17ea181521393ea0093ac4b9ab96907e05d6c263
Muhamad Amir <m.amirazmi@hotmail.com>
2023-03-31 00:08:09 +0800

    add param checker on projects page

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -1,3 +1,12 @@
+import type { LoaderFunction } from "react-router";
+
 export default function ProjectsPage() {
   return <div>ProjectsPage</div>;
 }
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const categoryParam = url.searchParams.get("category");
+  console.log(categoryParam);
+  return { message: "test" };
+};
