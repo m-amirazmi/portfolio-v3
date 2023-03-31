360e6947a6f5e062bc3b661ccf16d88369c50e22
Muhamad Amir <m.amirazmi@hotmail.com>
2023-03-31 19:05:14 +0800

    add simple animation

--- app/components/homepage/projects.tsx
+++ app/components/homepage/projects.tsx
@@ -5,8 +5,8 @@ export const Projects: React.FC<IPropsProjects> = ({ items }) => {
   return (
     <section className="pb-20">
       <div className="flex flex-col items-start justify-start md:flex-row md:items-center md:justify-between">
-        <h2 className="mb-4 text-2xl font-bold">Projects</h2>
-        <ul className="mb-4 flex flex-row items-center justify-start gap-2 text-sm">
+        <h2 className="mb-4 text-2xl font-bold md:mb-0">Projects</h2>
+        <ul className="mb-0 flex flex-row items-center justify-start gap-2 text-sm">
           <Link
             to="?p=react"
             className="rounded bg-zinc-50 px-3 py-1 hover:bg-teal-700 hover:bg-opacity-5 hover:text-teal-700"
@@ -33,13 +33,13 @@ export const Projects: React.FC<IPropsProjects> = ({ items }) => {
           </Link>
         </ul>
       </div>
-      <div className="flex flex-row flex-nowrap gap-2 overflow-x-auto pb-4">
+      <div className="flex flex-row flex-nowrap gap-4 overflow-x-auto py-4">
         {items.map((i) => {
           return (
             <Link
               to=""
               key={i.id}
-              className="h-80 flex-[0_0_211.2px] bg-zinc-200 hover:text-teal-800"
+              className="h-80 flex-[0_0_204.8px] rounded-md bg-zinc-500 bg-opacity-10 transition duration-150 ease-out hover:-translate-y-1 hover:text-teal-800 hover:shadow-lg"
             >
               <div className="flex h-full w-full flex-col justify-end p-4">
                 <h4>{i.title}</h4>
